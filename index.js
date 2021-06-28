a10d9159e858b7d3792bd7e08ebee1b262e17241
Komal6627 <komalkilledar1996@gmail.com>
2021-06-28 23:41:36 +0530

    update function added

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const generateNewCard = (taskData) => `
     <div class="col-md-6 col-lg-4">
         <div class="card text-center">
             <div class="card-header d-flex justify-content-end gap-2">
-                <button type="button" class="btn btn-outline-success">
-                    <i class="fas fa-pencil-alt"></i>
+                <button type="button" class="btn btn-outline-success"  id = ${taskData.id}  onclick=" editCard.apply(this,arguments)">
+                    <i class="fas fa-pencil-alt"  id = ${taskData.id}  onclick=" editCard.apply(this,arguments)"></i>
                 </button>
                 <button type="button" class="btn btn-outline-danger"  id = ${taskData.id}  onclick=" deletCard.apply(this,arguments)">
                                 <i class="fas fa-trash-alt" id = ${taskData.id} onclick=" deletCard.apply(this,arguments)"></i>
@@ -29,7 +29,7 @@ const generateNewCard = (taskData) => `
                             <a href="#" class="btn btn-primary">${taskData.taskType}</a>
                         </div>
                         <div class="card-footer">
-                            <button type="button" class="btn btn-outline-primary float-end">
+                            <button type="button"  id = ${taskData.id} class="btn btn-outline-primary float-end">
                                 Open Task
                             </button>
                         </div>
@@ -54,6 +54,7 @@ const loadInitialCardData = () => {
         globalStore.push(cardObject);
     })
 };
+
     
 //Save changes in model
 
@@ -95,3 +96,81 @@ const deletCard = (event) => {
 
 };
 
+//edit
+
+const editCard = (event) => {
+    
+    event = window.event;
+
+    //id
+    const targetID = event.target.id;
+    const tagname = event.target.tagName;
+
+    let parentElement;
+
+    if(tagname === "BUTTON"){
+        parentElement = event.target.parentNode.parentNode;
+    }
+    else{
+        parentElement = event.target.parentNode.parentNode.parentNode;
+    }
+
+    let taskTitle =parentElement.childNodes[5].childNodes[1];
+    let taskDescription = parentElement.childNodes[5].childNodes[3];
+    let taskType = parentElement.childNodes[5].childNodes[5];
+    let submitButton = parentElement.childNodes[7].childNodes[1];
+
+    taskTitle.setAttribute("contenteditable","true");
+    taskDescription.setAttribute("contenteditable","true");
+    taskType.setAttribute("contenteditable","true");
+    submitButton.setAttribute("onclick",
+    "saveEditChanges.apply(this.arguments)"
+    );
+    submitButton.innerHTML = "Save Change";
+
+};
+
+const saveEditChanges = (event) =>{
+    event = window.event;
+
+    //id
+    const targetID = event.target.id;
+    const tagname = event.target.tagName;
+
+    let parentElement;
+
+    if(tagname === "BUTTON"){
+        parentElement = event.target.parentNode.parentNode;
+    }
+    else{
+        parentElement = event.target.parentNode.parentNode.parentNode;
+    }
+
+    let taskTitle =parentElement.childNodes[5].childNodes[1];
+    let taskDescription = parentElement.childNodes[5].childNodes[3];
+    let taskType = parentElement.childNodes[5].childNodes[5];
+    let submitButton = parentElement.childNodes[7].childNodes[1];
+
+    const updatedData ={
+        taskTitle: taskTitle.innerHTML,
+        taskType: taskType.innerHTML,
+        taskDescription: taskDescription.innerHTML,
+    };
+   
+    globalStore = globalStore.map((task) =>{
+        if(task.id === targetID){
+           return {
+            id: task.id,
+            imageurl:task.imageurl,
+            taskTitle:updatedData.taskTitle,
+            taskType:updatedData.taskType,
+            taskDescription:updatedData.taskDescription,
+           };
+        }
+        return task;
+    }
+    );
+    localStorage.setItem("tasky", JSON.stringify({cards:globalStore}));
+
+
+};
